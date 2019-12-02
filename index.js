522005d06eb4564f48eb82fdfc63c79bdec4a3a2
shirai ryohei <shirai.ryohei0302@gmail.com>
2019-12-02 23:00:15 +0900

    modified: index.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const path = require('path')
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(`${__dirname}/index.html`))
-});
+})
 
 app.get('/Astronaut.glb', function(req, res) {
   res.sendFile(path.join(`${__dirname}/Astronaut.glb`))
-});
+})
 
 app.get('/Astronaut.usdz', function(req, res) {
   res.sendFile(path.join(`${__dirname}/Astronaut.usdz`))
-});
+})
 
 app.listen(3000, () => console.log('Listening on: http://localhost:3000'))
